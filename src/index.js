56f4462bc7502e75ec48a9bf4888270c28715305
Jacobo Saldarriaga <jacobosaldarriaga07@gmail.com>
2021-06-27 18:36:55 -0500

    morgan y nodemon trabajando correctamente

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
+
 
 const vs = '/api/v1/';
 
 // Middlewares
 app.use(express.json());
+app.use(morgan("combined"));
 
 app.set('port', process.env.PORT || 3000);
 
